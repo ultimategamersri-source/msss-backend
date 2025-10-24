c5b75c48658dd91bf413e1c1ea9c739f4fdc3531
ultimategamersri-source <ultimategamersri@gmail.com>
2025-10-24 23:58:26 +0530

    Update main.js

--- www.moderncbsechennai.com/js/main.js
+++ www.moderncbsechennai.com/js/main.js
@@ -1,6 +1,8 @@
 //const API = "https://msss-backend-665722959305.asia-south1.run.app";  // Cloud Run backend URL
 const API = "/api";
 // Example test call
+fetch(`${API}/chat`, {...})
+fetch(`${API}/ask`, {...})
 fetch(`${API}/health`)
   .then(res => res.json())
   .then(data => console.log("Backend connected:", data))
